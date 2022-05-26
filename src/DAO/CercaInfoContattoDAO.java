1a4989478f9c30a5ffea84a27e2b25696ca10c84
riioKen <40353984+sckipp@users.noreply.github.com>
2022-05-26 16:38:42 +0200

    Delete src directory

--- src/DAO/CercaInfoContattoDAO.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import Classi.Contatti;
-
-import java.sql.SQLException;
-
-public interface CercaInfoContattoDAO {
-    Contatti cercaInfoContatti(String nome) throws SQLException;
-}
--- src/DAO/EliminaContattoDAO.java
+++
@@ -1,8 +0,0 @@
-package DAO;
-
-import java.sql.SQLException;
-
-public interface EliminaContattoDAO {
-
-    void eliminaContatto(String cellulare) throws SQLException;
-}
