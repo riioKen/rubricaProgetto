2c7dd59e266c138bdab14f98b4382d1171d9ef57
riioKen <domenicovitale69@gmail.com>
2022-06-17 14:35:51 +0200

    Creazione gui RicercaContatti. Aggiunta di icone varie

---
+++ src/Gui/RicercaContatti.java
@@ -0,0 +1,11 @@
+package Gui;
+
+import Controller.Controller;
+
+public class RicercaContatti {
+
+    Controller control;
+    public RicercaContatti(Controller controller){
+        control = controller;
+    }
+}
